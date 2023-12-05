f6099d909a57459db8c78b2ff622b53b7143d81a
JOJO <978940343@qq.com>
2023-12-05 19:29:17 +0800

    chore: update npm

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,4 @@
-import typescript from '@rollup/plugin-typescript';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-import babel from '@rollup/plugin-babel';
+import common from '@rosmarinus/common-plugins';
 
 const external = ['@nestjs/common', '@nestjs/common/interfaces'];
 
@@ -16,19 +12,7 @@ function getConfig(format, banner) {
       sourcemap: true,
     },
     external,
-    plugins: [
-      commonjs(),
-      resolve({
-        preferBuiltins: true,
-      }),
-      json(),
-      typescript({
-        tsconfig: './tsconfig.json',
-      }),
-      babel({
-        babelHelpers: 'bundled',
-      }),
-    ],
+    plugins: [common()],
   };
 }
 
