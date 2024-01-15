989936d5021b3ce19aa0001feae2f430cdd36e0f
JOJO <978940343@qq.com>
2024-01-15 15:39:14 +0800

    feat: change sql npm to knex wrap

--- src/modules/mysql/mysql.service.ts
+++ src/modules/mysql/mysql.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Pool, createPool, PoolOptions } from 'mysql2';
+import knex, { Knex } from 'knex';
 
 export interface MysqlInitParams {
   address: string;
@@ -11,57 +11,31 @@ export interface MysqlInitParams {
 
 @Injectable()
 export class MysqlService {
-  private mysqlPool?: Pool;
+  private knex?: ReturnType<typeof knex>;
 
-  public init(params: MysqlInitParams, options?: PoolOptions) {
-    this.mysqlPool = createPool({
-      host: params.address,
-      port: params.port,
-      user: params.username,
-      password: params.password,
-      database: params.database,
-      waitForConnections: true,
-      connectionLimit: 10,
-      maxIdle: 10,
-      idleTimeout: 60000,
-      queueLimit: 0,
-      enableKeepAlive: true,
-      keepAliveInitialDelay: 0,
+  public init(params: MysqlInitParams, options?: Knex.Config) {
+    this.knex = knex({
+      client: 'mysql2',
+      connection: {
+        host: params.address,
+        port: params.port,
+        user: params.username,
+        password: params.password,
+        database: params.database,
+      },
+      pool: {
+        min: 1,
+        max: 10,
+      },
       ...options,
     });
   }
 
-  public async getMysqlClient() {
-    if (!this.mysqlPool) {
+  public getMysqlClient(): ReturnType<typeof knex> {
+    if (!this.knex) {
       throw new Error('mysql 未初始化');
     }
 
-    return this.mysqlPool;
-  }
-
-  public async query(sql: string): Promise<any> {
-    const conn = await this.getMysqlClient();
-
-    return new Promise<any>((resolve, reject) => {
-      conn.query(sql, (error, results) => {
-        if (error) {
-          return reject(error);
-        }
-
-        return resolve(results);
-      });
-    });
-  }
-
-  public async end() {
-    return new Promise<void>((resolve, reject) => {
-      this.mysqlPool?.end((err) => {
-        if (err) {
-          return reject(err);
-        }
-
-        return resolve();
-      });
-    });
+    return this.knex;
   }
 }
